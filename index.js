bd9f9557447c066405c4c6844524ee70b53d483c
joshua04d <145067673+joshua04d@users.noreply.github.com>
2023-10-08 05:54:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+new TypeIt("#multipleStrings", {
+    strings: ["This is a great string.", "But here is a better one."],
+    speed: 50,
+    waitUntilVisible: true,
+  }).go();
+
+
+
+
+//   cards
